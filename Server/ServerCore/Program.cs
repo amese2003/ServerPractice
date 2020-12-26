6cb35f5702323f303a9205d977fa5ed2f2e257f4
Yong Ju Kwak <amese1225@gmail.com>
2020-12-26 15:31:45 +0900

    Context swithching

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -22,6 +22,11 @@ namespace ServerCore
 
                 if(Interlocked.CompareExchange(ref _locked, desired, expected) == expected)                    
                     break;
+
+
+                //Thread.Sleep(1); // 무조건 휴식 => 무조건 1ms 휴식
+                //Thread.Sleep(0); // 조건부 휴식 => 나보다 우선순위가 낮으면 양보 불가 == 나보다 우선순위가 같거나 높은 쓰레드가 없으면 다시 본인한테
+                Thread.Yield();  // 관대한 양보 => 관대하게 양보하니, 지금 실행 가능한 쓰래드가 있으면 실행, 없으면 남은시간 소진
             }
 
 
