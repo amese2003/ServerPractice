3919db51505797f1b8afafffb3dff2aeb345d932
Yong Ju Kwak <amese1225@gmail.com>
2021-01-01 12:56:14 +0900

    서버쪽에서 클라이언트 세션 OnRecvPacket 세분화, 클라이언트쪽에서의 ServerSession을 program에서 분리, unsafe 코드 사용

--- Server/Server/ClientSession.cs
+++ Server/Server/ClientSession.cs
@@ -54,8 +54,34 @@ namespace Server
 
         public override void OnRecvPacket(ArraySegment<byte> buffer)
         {
+            int pos = 0;
+
             ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
-            ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + 2);
+            pos += 2;
+            ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + pos);
+            pos += 2;
+
+            switch ((PacketID)id)
+            {
+                case PacketID.PlayerInfoReq:
+                    long playerId = BitConverter.ToInt64(buffer.Array, buffer.Offset + pos);
+                    pos += 8;
+                    break;
+
+                case PacketID.PlayerInfoOk:
+                    int hp = BitConverter.ToInt32(buffer.Array, buffer.Offset + pos);
+                    pos += 4;
+                    int attack = BitConverter.ToInt32(buffer.Array, buffer.Offset + pos);
+                    pos += 4;
+                    break;
+                default:
+                    break;
+            }
+
+
+
+            //ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
+            //ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + 2);
             Console.WriteLine($"RecvPacketId : {id}, Size : {size}");
         }
 
